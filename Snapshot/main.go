80f1dfd168053224e6a65281c6c18648785d222c
Andrey <folganoid@yahoo.com>
2022-07-25 13:01:06 +0300

    Snapshot

---
+++ Snapshot/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"./pkg"
+	"fmt"
+)
+
+func main() {
+	storage := &pkg.Guardian{
+		Items: make([]*pkg.Snapshot, 0),
+	}
+	creator := &pkg.Creator{
+		State: "A",
+	}
+
+	fmt.Printf("Creator current State %s\n", creator.GetState())
+	storage.Add(creator.Create())
+
+	creator.SetState("B")
+	fmt.Printf("Creator current State %s\n", creator.GetState())
+	storage.Add(creator.Create())
+
+	creator.SetState("C")
+	fmt.Printf("Creator current State %s\n", creator.GetState())
+	storage.Add(creator.Create())
+
+	creator.Restore(storage.Get(1))
+	fmt.Printf("Creator current State %s\n", creator.GetState())
+	creator.Restore(storage.Get(0))
+	fmt.Printf("Creator current State %s\n", creator.GetState())
+}
